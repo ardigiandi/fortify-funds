5f9849924a6a1445feab6780f74ab1c2b1575808
ardigiandi <ardiansyahg9080@gmail.com>
2024-11-02 20:01:43 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "./public/**/*.html",
+    "./index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
