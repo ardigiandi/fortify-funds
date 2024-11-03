cf73764c2a5d02aa0ea44072252ee28e391a9acf
ardigiandi <ardiansyahg9080@gmail.com>
2024-11-03 16:07:13 +0700

    add : opportunities

--- src/script/index.js
+++ src/script/index.js
@@ -14,7 +14,7 @@ investImages.forEach((img) => {
 });
 
 // Pilih semua elemen card
-const cards = document.querySelectorAll('.card');
+const cards = document.querySelectorAll(".card");
 
 // Buat observer untuk mengamati elemen yang masuk/keluar dari viewport
 const observer = new IntersectionObserver(
@@ -22,12 +22,12 @@ const observer = new IntersectionObserver(
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
         // Tambahkan kelas animasi ketika elemen masuk viewport
-        entry.target.classList.add('animate-fade-up');
-        entry.target.classList.remove('opacity-0'); // Hilangkan opacity-0
+        entry.target.classList.add("animate-fade-up");
+        entry.target.classList.remove("opacity-0"); // Hilangkan opacity-0
       } else {
         // Kembali ke kondisi awal jika elemen keluar dari viewport
-        entry.target.classList.remove('animate-fade-up');
-        entry.target.classList.add('opacity-0');
+        entry.target.classList.remove("animate-fade-up");
+        entry.target.classList.add("opacity-0");
       }
     });
   },
@@ -38,9 +38,6 @@ const observer = new IntersectionObserver(
 cards.forEach((card) => {
   observer.observe(card);
 });
-
-
-
 
 // navbar dropdown
 document.addEventListener("DOMContentLoaded", function () {
@@ -57,6 +54,22 @@ document.addEventListener("DOMContentLoaded", function () {
       !dropdownmenu.contains(event.target)
     ) {
       dropdownmenu.classList.add("hidden");
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Dapatkan path URL halaman saat ini
+  const currentUrl = window.location.pathname;
+
+  // Pilih semua elemen link dengan kelas "nav-link"
+  const menuItems = document.querySelectorAll(".nav-link");
+
+  // Loop melalui setiap item menu
+  menuItems.forEach((item) => {
+    // Jika href item menu cocok dengan URL halaman saat ini, tambahkan kelas aktif
+    if (item.getAttribute("href") === currentUrl) {
+      item.classList.add("text-blue-600", "font-bold");
     }
   });
 });
