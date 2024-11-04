f71538fde2cf3b4502b797cc39a0477f9a78a5d9
ardigiandi <ardiansyahg9080@gmail.com>
2024-11-04 20:21:37 +0700

    add : fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "index.html", "src/**/*.{html,js}"
+    "./index.html", // Mengarahkan ke index.html di root proyek
+    "./src/**/*.{html,js}", // Mencakup semua file HTML dan JavaScript di folder src
   ],
   theme: {
     extend: {
       colors: {
-        'abu': '#EBEEF1',
-        'biru': '#304966'
+        abu: "#EBEEF1", // Warna custom abu-abu
+        biru: "#304966", // Warna custom biru
       },
       keyframes: {
-        'fade-up': {
-          '0%': { opacity: '0', transform: 'translateY(20px)' },
-          '100%': { opacity: '1', transform: 'translateY(0)' },
+        "fade-up": {
+          "0%": { opacity: "0", transform: "translateY(20px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
         },
       },
       animation: {
-        'fade-up': 'fade-up 0.8s cubic-bezier(0.22, 1, 0.36, 1) forwards',
+        "fade-up": "fade-up 0.8s cubic-bezier(0.22, 1, 0.36, 1) forwards",
       },
     },
   },
   plugins: [],
-}
-
+};
