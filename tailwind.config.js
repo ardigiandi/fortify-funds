8ea5d0910c89ef512391db209db817c49a2daee2
ardigiandi <ardiansyahg9080@gmail.com>
2024-11-04 20:13:34 +0700

    add : fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js,jsx,ts,tsx}",
-    "./public/**/*.html",
-    "./index.html"
+    "index.html", "src/**/*.{html,js}"
   ],
   theme: {
     extend: {
